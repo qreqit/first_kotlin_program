46e813c8bc82fb7f3bf1e6480e3485ba3a3a1b36
greqit <greqit@gmail.com>
2025-05-19 09:49:41 +0300

    .

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,4 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    println("Enter your city…")
-    val city = readLine()
-    println("User lives in $city")
 }
